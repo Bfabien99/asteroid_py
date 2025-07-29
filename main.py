6473f20291f10015c90d42b988ca8471c73ed58a
bfabien99 <fabienbrou99@gmail.com>
2025-07-29 09:33:56 +0000

    add shoot and bullet

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import sys
 import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
-import sys
+from shot import Shot
 
 
 def main():
@@ -14,8 +15,10 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
@@ -31,9 +34,10 @@ def main():
                 return
 
         updatable.update(dt)
+
         for asteroid in asteroids:
             if asteroid.collides_with(player):
-                print("Game Over!")
+                print("Game over!")
                 sys.exit()
 
         screen.fill("black")
