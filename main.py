7660c80a69bed9efa93c9d13057080a69853ac4d
bfabien99 <fabienbrou99@gmail.com>
2025-07-29 09:00:44 +0000

    add player rotation

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
+
         screen.fill("black")
         player.draw(screen)
-        
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
